36ab419278be2adcba2786be9ccc7644bed97a13
sschneeberg <simone.m.schneeberg@gmail.com>
2021-01-07 14:42:27 -0500

    reverse

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,11 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+def revString(str):
+    newStr = ''
+    for s in str:
+        newStr = s + newStr
+    return newStr
+
+print(revString('Hello this is backwards now!'))
